3fdab80803bcd8b7a014db12bd3a17d2e47ac7b3
Jonas <st171703@stud.uni-stuttgart.de>
2024-02-08 12:14:20 +0100

    update model_v2.py

--- utils/EMOCA/emoca_video_threading.py
+++ utils/EMOCA/emoca_video_threading.py
@@ -1,55 +1,64 @@
-import argparse
-import glob
-import os
-from pathlib import Path
+import threading
 
-import torch
-import torch.multiprocessing as mp
-from tqdm import auto
-
-import gdl
-from gdl.datasets.ImageTestDataset import TestData
-from gdl_apps.EMOCA.utils.io import save_obj
-from gdl_apps.EMOCA.utils.io import test
 from gdl_apps.EMOCA.utils.load import load_model
+from gdl.datasets.ImageTestDataset import TestData
+import gdl
+import numpy as np
+import os
+import torch
+from skimage.io import imsave
+from pathlib import Path
+from tqdm import auto
+import argparse
+from gdl_apps.EMOCA.utils.io import save_obj, save_images, save_codes, test
+from concurrent.futures import ThreadPoolExecutor
+import glob
+
+# Define a lock to ensure thread safety
+video_processing_lock = threading.Lock()
 
 
-def process_video_chunk(chunk_data):
-    """Processes a specified chunk of a video using a single GPU."""
-    video_path, start_frame, end_frame, output_folder, model_name, path_to_models, mode, save_images, save_codes, save_mesh, process_id, gpu_id = chunk_data
-
-    with torch.cuda.device(gpu_id):
+def process_video(video_path, output_folder, model_name, path_to_models, mode, save_images, save_codes, save_mesh):
+    with video_processing_lock:
         emoca, conf = load_model(path_to_models, model_name, mode)
         emoca.cuda()
         emoca.eval()
 
-        dataset = TestData(video_path, face_detector="fan", max_detection=20, start_frame=start_frame,
-                           end_frame=end_frame)
+        dataset = TestData(video_path, face_detector="fan", max_detection=20)
 
-        for j in auto.tqdm(range(len(dataset)),
-                           desc=f"Processing chunk ({process_id}:{gpu_id}) {os.path.basename(video_path)} ({start_frame}-{end_frame})"):
-            batch = dataset[j]
+        for i in auto.tqdm(range(len(dataset))):
+            batch = dataset[i]
             vals, visdict = test(emoca, batch)
 
             current_bs = batch["image"].shape[0]
 
-            for i in range(current_bs):
-                name = batch["image_name"][i]
+            for j in range(current_bs):
+                name = batch["image_name"][j]
                 sample_output_folder = Path(output_folder) / name
                 sample_output_folder.mkdir(parents=True, exist_ok=True)
-                save_obj(emoca, str(sample_output_folder / f"{video_path.replace('/', '_')}_mesh.obj"), vals, i)
+
+                if save_mesh:
+                    save_obj(emoca, str(sample_output_folder / f"{video_path.replace('/', '_')}_mesh.obj"), vals, j)
+                if save_images:
+                    save_images(output_folder, name, visdict, with_detection=True, i=j)
+                if save_codes:
+                    save_codes(Path(output_folder), name, vals, i=j)
+
+        print(f"Processing of {video_path} is complete")
 
 
-def worker(queue):
-    while True:
-        chunk_data = queue.get()
-        if chunk_data is None:
-            break
-        process_video_chunk(chunk_data)
+def process_videos_in_thread(video_paths, output_folder, model_name, path_to_models, mode, save_images, save_codes,
+                             save_mesh):
+    with ThreadPoolExecutor(max_workers=2) as executor:
+        futures = [executor.submit(process_video, video, output_folder, model_name, path_to_models, mode, save_images,
+                                   save_codes, save_mesh) for video in video_paths]
+
+        # Wait for all threads to finish
+        for future in futures:
+            future.result()
 
 
 def main():
-    # Argument parsing
     parser = argparse.ArgumentParser()
     parser.add_argument('--input_directory', type=str, default='videos_directory',
                         help="Input directory containing video files.")
@@ -58,51 +67,24 @@ def main():
     parser.add_argument('--model_name', type=str, default='EMOCA_v2_lr_mse_20', help='Name of the model to use.')
     parser.add_argument('--path_to_models', type=str,
                         default=str(Path(gdl.__file__).parents[1] / "assets/EMOCA/models"))
-    parser.add_argument('--save_images', type=bool, default=False, help="If true, output images will be saved")
+    parser.add_argument('--save_images', type=bool, default=True, help="If true, output images will be saved")
     parser.add_argument('--save_codes', type=bool, default=False,
                         help="If true, output FLAME values for shape, expression, jaw pose will be saved")
-    parser.add_argument('--save_mesh', type=bool, default=True, help="If true, output meshes will be saved")
+    parser.add_argument('--save_mesh', type=bool, default=False, help="If true, output meshes will be saved")
     parser.add_argument('--mode', type=str, default='detail', help="coarse or detail")
 
     args = parser.parse_args()
 
-    num_processes = min(mp.cpu_count(), len(torch.cuda.devices()))
-    num_gpus_per_process = 1
+    input_directory = args.input_directory
+    output_folder = args.output_folder
+    model_name = args.model_name
+    path_to_models = args.path_to_models
+    mode = args.mode
 
-    video_paths = glob.glob(os.path.join(args.input_directory, '*.mp4'))
+    videos = glob.glob(os.path.join(input_directory, '*.mp4'))
 
-    chunk_size = 64 
-    total_chunks = 0
-    for video_path in video_paths:
-        video_length_frames = len(TestData(video_path)) * 25  # Assuming 25fps
-        total_chunks += (video_length_frames + chunk_size - 1) // chunk_size
-
-    queue = mp.Queue()
-    processes = []
-    for _ in range(num_processes):
-        p = mp.Process(target=worker, args=(queue,))
-        processes.append(p)
-        p.start()
-
-    chunk_id = 0
-    for video_path in video_paths:
-        video_length_frames = len(TestData(video_path)) * 25
-        for i in range((video_length_frames + chunk_size - 1) // chunk_size):
-            start_frame = i * chunk_size
-            end_frame = min((i + 1) * chunk_size - 1, video_length_frames - 1)
-            process_id = chunk_id % num_processes
-            gpu_id = chunk_id // (num_processes * num_gpus_per_process) % len(torch.cuda.devices())
-            chunk_data = (
-                video_path, start_frame, end_frame, args.output_folder, args.model_name, args.path_to_models, args.mode,
-                args.save_images, args.save_codes, args.save_mesh, process_id, gpu_id)
-            queue.put(chunk_data)
-            chunk_id += 1
-
-    for _ in range(num_processes):
-        queue.put(None)
-
-    for p in processes:
-        p.join()
+    process_videos_in_thread(videos, output_folder, model_name, path_to_models, mode, args.save_images, args.save_codes,
+                             args.save_mesh)
 
 
 if __name__ == '__main__':
