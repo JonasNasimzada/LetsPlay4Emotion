8bf1d2c89e82d03686884e7f4754113357922594
Jonas <st171703@stud.uni-stuttgart.de>
2024-02-08 14:02:20 +0100

    update model_v2.py

--- slurm/train_model_binary.sh
+++ slurm/train_model_binary.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 #SBATCH --time 6-1
 #SBATCH --partition GPUampere
-#SBATCH --gpus=3
+#SBATCH --gpus=2
 #SBATCH --cpus-per-gpu=8
-srun python ../model/model_v2.py --mode train --type binary --version 1 --devices 3
+srun python ../model/model_v2.py --mode train --type binary --version 1 --devices 2
