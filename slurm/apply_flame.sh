e7a255de81710852331b6f0c0488203895d18259
Jonas <st171703@stud.uni-stuttgart.de>
2024-02-15 14:18:02 +0100

    add copying files threads

--- slurm/apply_flame.sh
+++ slurm/apply_flame.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 #SBATCH --time 2-1
-#SBATCH --cpus-per-gpu=64
+#SBATCH --cpus-per-task=64
 srun python ../FFHQ-UV/apply_uv_threading.py \
 --input_directory /groups/constantin_students/jnasimzada/all_videos/ \
 --thread_num 1
