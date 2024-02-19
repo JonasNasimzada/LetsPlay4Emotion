e5143ef00d6e7affbd235a755f5006058010c256
Jonas <st171703@stud.uni-stuttgart.de>
2024-02-19 16:25:46 +0100

    touch

--- slurm/gen_mesh_video/cpu/start_gen_mesh.sh
+++ slurm/gen_mesh_video/cpu/start_gen_mesh.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-for ((i = 1; i <= 20; i++)); do
+for ((i = 0; i <= 19; i++)); do
     echo "Processing number $i"
     sbatch --time 2-1 --cpus-per-task=64 --wrap="python generating_threading.py \
     --blend_file mesh_sequence_v3.blend \
