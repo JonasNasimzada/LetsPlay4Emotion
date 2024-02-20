5cdb4c8bc13e39bafa9efe11408047681677000f
Jonas <st171703@stud.uni-stuttgart.de>
2024-02-20 12:16:35 +0100

    touch

--- slurm/gen_mesh_video/cpu/start_gen_mesh.sh
+++ slurm/gen_mesh_video/cpu/start_gen_mesh.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-echo "Enter list of uv_materials (None, Person_1,...) (separated by spaces):"
+echo "Enter list of uv-textures (None, Person_0,...) (separated by spaces):"
 read -r uv_material
 
 echo "Enter the camera view (Front or Side) (separated by spaces):"
@@ -16,9 +16,18 @@ read -r dependency
 IFS=' ' read -r -a uv_material_list <<< "$uv_material"
 IFS=' ' read -r -a camera_list <<< "$camera"
 
+echo "Processing uv-textures: ${uv_material_list[*]}"
+echo "Processing camera view: ${camera_list[*]}"
+echo "Processing amount of nodes: $number"
+echo "Processing camera view: ${camera_list[*]}"
+if [[ -n "$dependency" ]]; then
+        echo "Processing dependency: $dependency"
+    else
+        echo "No dependency entered"
+    fi
 
 for ((i = 1; i <= number; i++)); do
-    echo "Processing number $i"
+
     sbatch_command="sbatch --time 1-1 --cpus-per-task=64"
     if [[ -n "$dependency" ]]; then
         sbatch_command+=" --dependency=$dependency"
