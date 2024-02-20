1b4b0b7215885a6c7aa9281855c64aef796fa604
Jonas <st171703@stud.uni-stuttgart.de>
2024-02-20 20:38:58 +0100

    touch

--- slurm/gen_mesh_video/cpu/start_gen_mesh.sh
+++ slurm/gen_mesh_video/cpu/start_gen_mesh.sh
@@ -22,7 +22,6 @@ IFS=' ' read -r -a camera_list <<< "$camera"
 echo "Processing uv-textures: ${uv_material_list[*]}"
 echo "Processing camera views: ${camera_list[*]}"
 echo "Processing amount of nodes: $number"
-echo "Processing camera view: ${camera_list[*]}"
 if [[ -n "$dependency" ]]; then
         echo "Processing dependency: $dependency"
     else
@@ -34,7 +33,7 @@ for ((i = 1; i <= number; i++)); do
 
     sbatch_command="sbatch --time 1-1 --cpus-per-task=64"
     if [[ -n "$dependency" ]]; then
-        sbatch_command+=" --dependency= afterok:$dependency"
+        sbatch_command+=" --dependency afterok:$dependency"
     fi
     sbatch_command+=" --wrap=\"python generating_threading.py \
         --blend_file mesh_sequence_v$blend_file_version.blend \
