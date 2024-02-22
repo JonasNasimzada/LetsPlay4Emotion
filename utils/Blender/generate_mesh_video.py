9746036fca14144714012a951efd997e50469aee
Jonas <st171703@stud.uni-stuttgart.de>
2024-02-22 13:27:13 +0100

    touch

--- utils/Blender/generate_mesh_video.py
+++ utils/Blender/generate_mesh_video.py
@@ -38,7 +38,7 @@ def load_and_render_mesh(input_path, file_path, output_path, uv_material_list, c
     # render mesh for different uv-textures and camera-positions
     for uv_material in uv_material_list:
         uv_texture = blend.materials[uv_material]
-        if uv_material is "None":
+        if uv_material == "Default":
             uv_texture = blend.materials["Default OBJ"]
         head.active_material = uv_texture
         for camera_position in camera_position_list:
