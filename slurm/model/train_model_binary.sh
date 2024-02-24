ed061a34b2f8cf0348968434ac799f0582d49649
Jonas <st171703@stud.uni-stuttgart.de>
2024-02-24 21:57:01 +0100

    touch

--- slurm/model/train_model_binary.sh
+++ slurm/model/train_model_binary.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 #SBATCH --time 6-1
 #SBATCH --partition GPUampere
-#SBATCH --gpus=1
+#SBATCH --gpus=2
 #SBATCH --cpus-per-gpu=8
-srun python ../model/model_v2.py --mode train --type binary --version 1 --devices 1 --logger_comment "$1"
+srun python ../model/model_v2.py --mode train --type binary --version 1 --devices 2 --logger_comment "$1"
