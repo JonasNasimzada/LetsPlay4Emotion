8ab25622d0e1c5b75ebc25de481a54a6bca0568e
Jonas <st171703@stud.uni-stuttgart.de>
2024-02-25 16:43:25 +0100

    touch

--- slurm/etc/create_video_threading.sh
+++ slurm/etc/create_video_threading.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 #SBATCH --time 2-1
-#SBATCH --cpus-per-gpu=64
+#SBATCH --cpus-per-task=64
 srun python create_video_threading.py \
 --input_directory /groups/constantin_students/jnasimzada/output_mesh \
 --output_dir /groups/constantin_students/jnasimzada/video_head \
