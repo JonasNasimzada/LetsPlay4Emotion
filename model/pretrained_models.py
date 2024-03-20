79a374edfcca8114c3a4756ba46acb2a5aed201a
Jonas <st171703@stud.uni-stuttgart.de>
2024-03-20 10:35:51 +0100

    touch

--- model/pretrained_models.py
+++ model/pretrained_models.py
@@ -9,6 +9,7 @@ class Resnet50_FER(nn.Module):
         super(Resnet50_FER, self).__init__()
         self.model = Resnet50_face_sfew_dag()
         self.model.load_state_dict(torch.load(weights_path))
+        self.model.conv1_conv = nn.Conv2d(414, 64, kernel_size=[7, 7], stride=(2, 2), padding=(3, 3))
         self.model.prediction = nn.Linear(in_features=2048, out_features=1, bias=True)
         #self.model.prediction_avg = nn.AvgPool2d(kernel_size=1, stride=[1, 1], padding=0)
 
