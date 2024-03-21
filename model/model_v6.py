33863f2845b64c232a09c1c40ec656c2ac9e1ab4
Jonas <st171703@stud.uni-stuttgart.de>
2024-03-21 09:26:03 +0100

    touch

--- model/model_v6.py
+++ model/model_v6.py
@@ -147,7 +147,6 @@ class NeuralNetworkModel(LightningModule):
             ImglistToTensor(),  # list of PIL images to (FRAMES x CHANNELS x HEIGHT x WIDTH) tensor
             transforms.Resize(256),  # Resize to 256x256
             transforms.CenterCrop(224),  # Center crop to 224x224
-            transforms.ToTensor(),  # Convert to tensor
             transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),  # Normalize
         ])
 
