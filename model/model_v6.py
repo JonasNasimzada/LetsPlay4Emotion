fcc9b84753e76fdc81289fc1d20d7ca45f4db8f6
Jonas <st171703@stud.uni-stuttgart.de>
2024-03-21 09:48:47 +0100

    touch

--- model/model_v6.py
+++ model/model_v6.py
@@ -88,11 +88,11 @@ class NeuralNetworkModel(LightningModule):
 
     def train_dataloader(self):
         preprocess = transforms.Compose([
-            ImglistToTensor(),  # list of PIL images to (FRAMES x CHANNELS x HEIGHT x WIDTH) tensor
             transforms.RandomResizedCrop(224, scale=(0.8, 1.0)),  # Random resized crop
             transforms.RandomHorizontalFlip(),  # Random horizontal flip
             transforms.RandomRotation(degrees=15),  # Random rotation
             transforms.ColorJitter(brightness=0.4, contrast=0.4, saturation=0.4, hue=0.1),  # Color jitter
+            ImglistToTensor(),  # list of PIL images to (FRAMES x CHANNELS x HEIGHT x WIDTH) tensor
             transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
         ])
 
@@ -144,9 +144,9 @@ class NeuralNetworkModel(LightningModule):
 
     def val_dataloader(self):
         preprocess = transforms.Compose([
-            ImglistToTensor(),  # list of PIL images to (FRAMES x CHANNELS x HEIGHT x WIDTH) tensor
             transforms.Resize(256),  # Resize to 256x256
             transforms.CenterCrop(224),  # Center crop to 224x224
+            ImglistToTensor(),  # list of PIL images to (FRAMES x CHANNELS x HEIGHT x WIDTH) tensor
             transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),  # Normalize
         ])
 
