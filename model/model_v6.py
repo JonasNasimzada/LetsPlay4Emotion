9cc74ff91e35e3d3cacb15a1d45b90f65de72077
Jonas <st171703@stud.uni-stuttgart.de>
2024-03-21 13:41:06 +0100

    touch

--- model/model_v6.py
+++ model/model_v6.py
@@ -120,10 +120,6 @@ class NeuralNetworkModel(LightningModule):
     def _common_step(self, batch, batch_idx):
         video, input_label = batch
         if self.model_type == "binary":
-            batch_size, frames, channels, height, width = video.shape
-            video = video.reshape(batch_size * frames, channels, height, width)
-            input_label = torch.mul(input_label, frames)
-            print(f"SHAAAPEPEPEPEPEE : {input_label.shape}")
             input_label = input_label.to(torch.float32).unsqueeze(1)
             output_network = self.forward(video)
             loss = self.loss(output_network, input_label)
