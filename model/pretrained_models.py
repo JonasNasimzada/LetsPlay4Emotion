673264169c92b2a81988507f75aa65f60a9e8a63
Jonas <st171703@stud.uni-stuttgart.de>
2024-03-22 08:01:38 +0100

    touch

--- model/pretrained_models.py
+++ model/pretrained_models.py
@@ -22,7 +22,8 @@ class Resnet50_FER_V2(nn.Module):
     def __init__(self, weights_path):
         super(Resnet50_FER_V2, self).__init__()
         self.model = Resnet50_face_sfew_dag()
-        self.model.load_state_dict(torch.load(weights_path))
+        loaded = self.model.load_state_dict(torch.load(weights_path))
+        print(f"is loaded : {loaded}")
         self.model.prediction = nn.Linear(in_features=2048, out_features=1, bias=True)
 
     def forward(self, imgs):
