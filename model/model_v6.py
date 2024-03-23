b9d1b160e05daa11c2fa80f0913129f59c373ca5
Jonas <st171703@stud.uni-stuttgart.de>
2024-03-23 07:42:12 +0100

    touch

--- model/model_v6.py
+++ model/model_v6.py
@@ -1,67 +1,43 @@
 import argparse
 import os
 
+import matplotlib.pyplot as plt
+import numpy as np
 import pandas as pd
-import pytorchvideo
 import seaborn as sns
 import torch
 import torch.nn as nn
-import torchvision.transforms.v2
-from matplotlib import pyplot as plt
 from pytorch_lightning import Trainer, LightningModule
 from pytorch_lightning.callbacks import ModelCheckpoint, LearningRateMonitor
 from pytorch_lightning.loggers import TensorBoardLogger
-from pytorchvideo.data import make_clip_sampler, labeled_video_dataset
-from pytorchvideo.transforms import (
-    ApplyTransformToKey,
-    UniformTemporalSubsample,
-)
-from pytorchvideo.transforms import (
-    RandomShortSideScale,
-    ShortSideScale
-)
+from tensorboardX import SummaryWriter
 from torch.optim import SGD
 from torch.optim.lr_scheduler import CosineAnnealingLR
 from torch.utils.data.sampler import WeightedRandomSampler
-from torch.utils.data import DataLoader
 from torchmetrics import Precision, Recall, F1Score, Accuracy, ConfusionMatrix, AUROC
 from torchvision.transforms.v2 import Compose, Lambda, RandomResizedCrop, RandomRotation, ColorJitter, Resize, \
-    CenterCrop, ToDtype, Normalize
+    CenterCrop, Normalize
 from torchvision.transforms.v2 import (
     RandomCrop,
     RandomHorizontalFlip
 )
-from torchvision.transforms._transforms_video import (
-    NormalizeVideo, CenterCropVideo
-)
-from torchvision.transforms.functional import to_pil_image, to_tensor
-from kinetics import Kinetics
-import numpy as np
-import matplotlib.pyplot as plt
-from tensorboardX import SummaryWriter
 
+from pretrained_models import Resnet50_FER_V2
 from video_dataset import VideoFrameDataset, ImglistToTensor
-from pretrained_models import Resnet50_FER, Resnet50_FER_V2
 
 
 class NeuralNetworkModel(LightningModule):
-    def __init__(self, num_classes, model_type, train_dataset_file, val_dataset_file, nn_model, augmentation_train,
-                 augmentation_val, clip_duration, batch_size, video_path_prefix, annotation_file_train,
+    def __init__(self, num_classes, model_type, nn_model, batch_size, video_path_prefix, annotation_file_train,
                  annotation_file_val):
         super().__init__()
         self.video_path_prefix = video_path_prefix
         self.annotation_file_train = annotation_file_train
         self.annotation_file_val = annotation_file_val
         self.model_type = model_type
-        self.train_dataset_file = train_dataset_file
-        self.val_dataset_file = val_dataset_file
-        self.augmentation_train = augmentation_train
-        self.augmentation_val = augmentation_val
         self.conv_layers = nn_model
         self.lr = 0.01
         self.batch_size = batch_size
         self.num_worker = 8
-        self.clip_duration = clip_duration
         self.num_classes = num_classes
 
         self.f1_score = F1Score(task=self.model_type, num_classes=self.num_classes)
@@ -230,7 +206,6 @@ class NeuralNetworkModel(LightningModule):
         self.logger.experiment.add_figure("val_confusion_matrix v3", _fig, self.current_epoch)
         writer.close()
 
-
         confusion_matrix_computed = self.confusion_matrix.compute().detach().cpu().numpy().astype(int)
         if self.model_type == "binary":
             df_cm = pd.DataFrame(confusion_matrix_computed, index=range(2), columns=range(2))
@@ -255,8 +230,6 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument("--mode", required=True, choices=['train', 'pred'])
     parser.add_argument("--type", required=True, choices=['binary', 'multi'])
-    parser.add_argument("--dataset_infix", default="", help='optional infix for choosing dataset')
-    parser.add_argument("--dataset_path", default="datasets")
     parser.add_argument("--logger_comment", default="")
     parser.add_argument("--version", default="", type=int)
     parser.add_argument("--devices", default=1, type=int)
@@ -273,14 +246,9 @@ if __name__ == '__main__':
 
     os.environ['TF_CPP_MIN_LOG_LEVEL'] = '1'
 
-    data_suffix = args.type
-    data_infix = ""
     classes = None
     version = "v1"
     metric = None
-
-    if args.dataset_infix != "":
-        data_infix = f"{args.dataset_infix}_"
 
     if args.version != "":
         version = f"v{args.version}_"
@@ -292,55 +260,15 @@ if __name__ == '__main__':
         classes = 5
         metric = "multiclass"
 
-    num_clips = 10
-    num_crops = 3
-    side_size = 256
-    mean = [0.45, 0.45, 0.45]
-    std = [0.225, 0.225, 0.225]
-    crop_size = 256
-    num_frames = 32
-    sampling_rate = 2
-    frames_per_second = 25
-
-    video_transform_train = ApplyTransformToKey(
-        key="video",
-        transform=Compose(
-            [
-                UniformTemporalSubsample(8),
-                Lambda(lambda x: x / 255.0),
-                # Normalize((0.45, 0.45, 0.45), (0.225, 0.225, 0.225)),
-                RandomShortSideScale(min_size=256, max_size=320),
-                RandomCrop(244),
-                RandomHorizontalFlip(p=0.5),
-            ]
-        ),
-    )
-
-    video_transform_val = ApplyTransformToKey(
-        key="video",
-        transform=Compose(
-            [
-                UniformTemporalSubsample(num_frames),
-                Lambda(lambda x: x / 255.0),
-                # Normalize(mean, std),
-                ShortSideScale(
-                    size=side_size
-                ),
-                CenterCropVideo(crop_size),
-            ]
-        ),
-    )
-    video_duration = (num_frames * sampling_rate) / frames_per_second
-
     model_resnet = Resnet50_FER_V2(args.model_ckpt)
 
-    checkpoint_dirpath = f'checkpoints_{version}{data_suffix}'
+    checkpoint_dirpath = f'checkpoints_{version}{args.type}'
     checkpoint_callback = ModelCheckpoint(monitor='val_loss', dirpath=checkpoint_dirpath,
-                                          filename=f"ckpt-{version}{data_infix}{data_suffix}" + '-{epoch:02d}-{val_loss:.2f}',
+                                          filename=f"ckpt-{version}{args.type}{args.logger_comment}" + '-{epoch:02d}-{val_loss:.2f}',
                                           save_last=True)
     lr_monitor = LearningRateMonitor(logging_interval='epoch')
 
-    logger_name = f"model_{version}{data_infix}{args.type}"
+    logger_name = f"model_{version}{args.type}{args.type}"
     logger = TensorBoardLogger("model_logger", name=logger_name,
                                version=f"version_{version}_{logger_name}_{args.logger_comment}")
     print(f"the logger name is: {logger_name}")
@@ -359,25 +287,15 @@ if __name__ == '__main__':
         logger=logger
     )
 
-    train_set = f"{args.dataset_path}/train-{data_infix}{data_suffix}.csv"
-    val_set = f"{args.dataset_path}/val-{data_infix}{data_suffix}.csv"
-
     model = NeuralNetworkModel(
         num_classes=classes,
         model_type=metric,
-        train_dataset_file=train_set,
-        val_dataset_file=val_set,
         nn_model=model_resnet,
-        augmentation_val=video_transform_val,
-        augmentation_train=video_transform_train,
-        clip_duration=video_duration,
         batch_size=args.batch_size,
         video_path_prefix=args.video_path_prefix,
         annotation_file_val=args.ann_val,
         annotation_file_train=args.ann_train
     )
-    print(f"this train set is gonna be used: {train_set}")
-    print(f"this val set is gonna be used: {val_set}")
 
     if args.mode == 'train':
         if args.ckpt != "":
