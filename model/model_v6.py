7a46c190e9864cecc27de37fb1da44a9c56b936c
Jonas <st171703@stud.uni-stuttgart.de>
2024-03-23 15:14:40 +0100

    touch

--- model/model_v6.py
+++ model/model_v6.py
@@ -101,8 +101,8 @@ class NeuralNetworkModel(LightningModule):
         else:
             input_label = input_label.to(torch.int64)
             output_network = self.forward(video)
-        # batch_size_and_frames, label = output_network.shape
-        # output_network = output_network.reshape(batch_size, frames, label)
+        batch_size_and_frames, label = output_network.shape
+        output_network = output_network.reshape(batch_size, label)
         # output_network = output_network.mean(1)
         loss = self.loss(output_network, input_label)
         return loss, output_network, input_label
