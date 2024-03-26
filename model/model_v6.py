df4de5a306b0ffdeb167ec1a873922849400298c
Jonas <st171703@stud.uni-stuttgart.de>
2024-03-26 10:57:51 +0100

    touch

--- model/model_v6.py
+++ model/model_v6.py
@@ -123,8 +123,6 @@ class NeuralNetworkModel(LightningModule):
         )
         if batch_idx % 100 == 0:
             x = x[0][:8]
-            import pdb
-            pdb.set_trace()
             grid = torchvision.utils.make_grid(x.view(-1, 1, 28, 28))
             self.logger.experiment.add_image("images", grid, self.global_step)
         return pred
